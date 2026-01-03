b8ceadf69c5a86ee8419d6162abddaf399b73ca6
Karnav Kmaleshkumar Prajapati <134150848+Karnav018@users.noreply.github.com>
2026-01-03 12:32:44 +0530

    Fevicon added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import Signin from "./pages/signin/signin";
-
+// import Signin from "./pages/signin/signin";
+// import Signup from "./pages/signup/signup";
+import Dashboard from "./pages/admin/dashboard.jsx";
 const App = () => {
   return (
     <>
-      <Signin />
+      <Dashboard />
     </>
   );
 };
